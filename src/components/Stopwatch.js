407a5ecf9e67ad120dfbf8c84b9b78591591a7d7
Justin Zhang <jz367132@gmail.com>
2020-02-18 09:52:45 -0500

    Add basic documentation

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import '../App.css';
 
 class Stopwatch extends Component {
+    /*
+    Constructor for the stopwatch
+    Initializes variables
+     */
     constructor(props) {
         super(props);
         this.state = {
@@ -13,15 +17,24 @@ class Stopwatch extends Component {
         };
 
     }
+    /*
+    Simple wrapper function to allow for one button to call two functions
+     */
     wrapperStartFunction = () =>{
-        this.startTimer()
+        this.startTimer();
         this.lapTimer()
-    }
+    };
+
+    /*
+    Function to start the timer
+     */
     startTimer = () => {
+        //Change states
         this.setState({
             timerOn: true,
             time: this.state.time,
         });
+        //Begin counting up
         this.timer = setInterval(() => {
             this.setState({
                 time: this.state.time + 10
@@ -29,24 +42,39 @@ class Stopwatch extends Component {
         }, 10);
     };
 
+    /*
+    Function to stop the timer
+     */
     stopTimer = () => {
+        //Changes states
         this.setState({
             timerOn: false,
             time: this.state.time
         });
+        //Stops the timer
         clearInterval(this.timer)
     };
 
+    /*
+    Function for stopwatch laps
+     */
     lapTimer = () => {
         this.setState({
+            //Calculates the time elapsed since last lap
             lapTimes: this.state.lapTimes.concat(this.state.time - this.state.previousLapTime),
+            //Updates state
             previousLapTime: this.state.time,
+            //Increments count
             numLaps: this.state.numLaps + 1,
         })
-    }
+    };
 
+    /*
+    Function to restart the timer
+     */
     resetTimer = () => {
         this.setState({
+            //Resets all variables, including clearing laps
             time: 0,
             previousLapTime: 0,
             numLaps: 0,
@@ -54,23 +82,33 @@ class Stopwatch extends Component {
         });
     };
 
+    /*
+    Function to render the stopwatch
+     */
     render() {
+        //Gets some variables
         const {time} = this.state;
         const lapTimes = this.state.lapTimes;
+
+        //Converts numerical time into readable times
         let centiseconds = ("0" + (Math.floor(time / 10) % 100)).slice(-2);
         let seconds = ("0" + (Math.floor(time / 1000) % 60)).slice(-2);
         let minutes = ("0" + (Math.floor(time / 60000) % 60)).slice(-2);
         let hours = ("0" + Math.floor(time / 3600000)).slice(-2);
 
+        //Begin creating auto-updating lap list
         const laps = lapTimes.map((step, lap) => {
+            //Converts numerical time into readable time (for laps)
             let lapCentiseconds = ("0" + (Math.floor(step / 10) % 100)).slice(-2);
             let lapSeconds = ("0" + (Math.floor(step / 1000) % 60)).slice(-2);
             let lapMinutes = ("0" + (Math.floor(step / 60000) % 60)).slice(-2);
             let lapHours = ("0" + Math.floor(step / 3600000)).slice(-2);
 
+            //Create text for each element of list
             const desc = lap ?
                 'Lap #' + lap+"  "+lapHours+":"+lapMinutes+":"+lapSeconds+":"+lapCentiseconds:
                 'Laps';
+            //Updates a list with each list element with the description for each lap
             return (
                 <div className="laps">
                     <li key={lap}>
@@ -80,12 +118,14 @@ class Stopwatch extends Component {
 
             );
         });
+        //Display for main timer
         return (
             <div className="Stopwatch">
                 <div className="Stopwatch-header">Stopwatch</div>
                 <div className="Stopwatch-display">
                     {hours} : {minutes} : {seconds} : {centiseconds}
                 </div>
+                <!-- Determines which button are shown, as well was what buttons do -->
                 {this.state.timerOn === false && this.state.time === 0 && (
                     <button onClick={this.wrapperStartFunction} >Start</button>
                 )}
@@ -101,6 +141,7 @@ class Stopwatch extends Component {
                 {this.state.timerOn === false && this.state.time > 0 && (
                     <button onClick={this.resetTimer}>Reset</button>
                 )}
+                <!-- Also displays the list of laps -->
                 <ul>{laps}</ul>
             </div>
     )
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,6 +3,8 @@ import '../App.css';
 
 class Timer extends Component {
 
+    //Constructor for the timer
+    //Initializes some variables
     constructor(props){
         super(props);
         this.state = {
@@ -12,21 +14,27 @@ class Timer extends Component {
         };
         this.handleChange = this.handleChange.bind(this);
     }
+    //Start timer function
     startTimer = () => {
+        //Catch null case
         if(isNaN(this.parseTime(this.state.time))) {
         }
         else {
+            //Update state variables
             this.setState({
                 timerOn: true,
                 time: this.state.time,
                 default: this.state.time,
             });
+            //Start timer countdown
             this.timer = setInterval(() => {
                 const numericalTime = this.parseTime(this.state.time) - 10;
+                //Normal countdown
                 if (numericalTime >= 0){
                     this.setState({
                         time: this.timeToString(numericalTime),
                 })
+                    //If time is up, stop timer and alert
                 } else {
                         clearInterval(this.timer);
                         this.setState({ timerOn: false });
@@ -37,20 +45,26 @@ class Timer extends Component {
         }
     };
 
+    //Function to stop the timer
     stopTimer = () => {
+        //Update state variables
         this.setState({
             timerOn: false,
             time: this.state.time
         });
+        //Stop countdown
         clearInterval(this.timer)
     };
 
+    //Function to reset the timer
     resetTimer = () => {
+        //Reset state variables
         this.setState({
             time: this.state.default,
             timerOn: this.state.timerOn,
         });
     };
+    //Function to convert numerical time into readable format
     timeToString = (time) =>{
         let centiseconds = ("0" + (Math.floor(time / 10) % 100)).slice(-2);
         let seconds = ("0" + (Math.floor(time / 1000) % 60)).slice(-2);
@@ -59,22 +73,28 @@ class Timer extends Component {
         let text = ""+hours+":"+minutes+":"+seconds+":"+centiseconds;
         return text;
     };
+
+    //Function to convert text/readable time into numerical format
     parseTime = (time) => {
         let hms = time.split(":");
         return hms[0] * 3600000 + hms[1] * 60000 + hms[2] * 1000 + hms[3]*10;
     };
 
+    //Helper function for changing the time
     handleChange(event) {
         this.setState({time: event.target.value});
     }
 
+    //Render Function
     render() {
         return (
+            //Display
             <div className="Timer">
                 <div className="Timer-header">Timer</div>
                 <div className="Timer-display">
                     <textarea className="Timer-box" disabled={this.state.timerOn} value={this.state.time} onChange={this.handleChange} />
                 </div>
+                <!-- Determines which buttons to show and also button functionality -->
                 {this.state.timerOn === false && (
                     <button onClick={this.startTimer}>Start</button>
                 )}
